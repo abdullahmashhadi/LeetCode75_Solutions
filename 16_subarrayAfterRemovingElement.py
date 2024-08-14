d6c981a2db3186e1db2e5d35e6cd1a71b68addb5
abdullahmashhadi <abdullah6blue@gmail.com>
2024-08-14 12:01:39 +0500

    problem#16 solved

--- 16_subarrayAfterRemovingElement.py
+++ 16_subarrayAfterRemovingElement.py
@@ -1,20 +1,20 @@
 class Solution:
-    def longestOnes(self, nums: list[int], k: int) -> int:
+    def longestSubarray(self, nums: list[int]) -> int:
+        num_zeros=0
         l=0
         max_w=0
-        num_zeros=0
         for r in range(len(nums)):
-            if nums[r]==0:
+            if (nums[r]==0):
                 num_zeros+=1
-            while num_zeros>k:
+            while num_zeros>1:
                 if nums[l]==0:
                     num_zeros-=1
                 l+=1
             w=r-l+1
             max_w=max(max_w,w)
-        return max_w
+        return max_w-1
 nums=list(map(int,input("Enter space separated array of 0s and 1s:\n").split()))
-k=int(input("Enter the number of 0s to flip:\n"))
 sol=Solution()
-output=sol.longestOnes(nums,k)
-print(f"max numbers of consecutive 1s subarray is: {output}")
+output=sol.longestSubarray(nums)
+print(f"max numbers of consecutive 1s subarray after deleting one 0 is: {output}")       
+        
